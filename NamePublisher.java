78c57f98e8706fd89cb5ab0c9a5236e93e7f5846
Ilnar <Downfallfast@gmail.com>
2013-11-01 23:13:10 +0400

    NamePublisher editing

--- NamePublisher.java
+++ NamePublisher.java
@@ -4,5 +4,6 @@ public class NamePublisher {
 	static ArrayList<String> nameList;
 	public static void main(String[] args) {
 		nameList = new ArrayList<String>();
+		nameList.add("Khaphizov Ilnar");
 	}
 }
